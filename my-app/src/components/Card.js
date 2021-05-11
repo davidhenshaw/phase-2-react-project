c4839c2a8f1c5bd6a651b8a4b52187d521ab9809
davidhenshaw <davidhenshaw2014@gmail.com>
2021-05-11 16:20:31 -0500

    Tweak card rendering; installed new packages

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 
 class Card extends React.Component {
 
+    renderActivities = () => 
+    {
+        return this.props.park.activities.map( (activity) => {
+            <li>{activity.name}</li>
+        })
+    }
+
+
     render(){
         return (
         <div className="card">
-            <h2>{this.props.park.fullName}</h2>
             <img src={this.props.park.images[0].url} alt="park"/>
-                <button className="like-btn">favorite ⭐️ </button>
+            <h2>{this.props.park.fullName}</h2>
+            <h3>{this.props.park.states}</h3>
+            <span>{this.renderActivities()}</span>
+            <p>{this.props.park.description}</p>
+            <button className="like-btn">favorite ⭐️ </button>
         </div>
         );
     }
