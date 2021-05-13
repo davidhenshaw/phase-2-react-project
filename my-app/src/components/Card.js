037b3fd172fc5a12a4c3a9e3c401695107ccf384
davidhenshaw <davidhenshaw2014@gmail.com>
2021-05-13 17:54:15 -0500

    Added and styled links on cards

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -14,11 +14,21 @@ class Card extends React.Component {
         return (
         <div className="card" key={this.props.park.id}>
             <img className="park-img"src={this.props.park.images[0].url} alt="park"/>
-            <h2>{this.props.park.fullName}</h2>
-            <h3 className="states">{this.props.park.states}</h3>
-            <span>{this.renderActivities()}</span>
-            <p>{this.props.park.description}</p>
-            <button className="like-btn"onClick={()=>this.props.handleClick(this.props.park)}>favorite ⭐️ </button>
+            <a href={this.props.park.url} target="_blank">
+                <h2>
+                    {this.props.park.fullName}
+                </h2>
+            </a>
+            <h3 className="states">
+                {this.props.park.states}
+            </h3>
+            <span>
+                {this.renderActivities()}
+            </span>
+            <p>
+                {this.props.park.description}
+            </p>
+            <button className="like-btn"onClick={()=>this.props.handleClick(this.props.park)}>Favorite ⭐️ </button>
         </div>
         );
     }
