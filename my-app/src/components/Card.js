6484e7a1e6d22b923d6d4497f63681527ef3ce72
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-05-13 13:49:20 -0500

    card styling

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -15,14 +15,13 @@ class Card extends React.Component {
         <div className="card" key={this.props.park.id}>
             <img className="park-img"src={this.props.park.images[0].url} alt="park"/>
             <h2>{this.props.park.fullName}</h2>
-            <h3>{this.props.park.states}</h3>
+            <h3 className="states">{this.props.park.states}</h3>
             <span>{this.renderActivities()}</span>
             <p>{this.props.park.description}</p>
             <button className="like-btn"onClick={()=>this.props.handleClick(this.props.park)}>favorite ⭐️ </button>
         </div>
         );
     }
-
 }
 
 export default Card;
